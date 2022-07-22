892e3f57337d855492ca540d10629c746e0228af
andrew <s.s.student@khai.edu>
2022-07-23 01:02:52 +0300

    refactor basic structure and add mouse liseners

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -1,15 +1,68 @@
-import React from "react";
+import React, { Component, createRef } from "react";
 import styles from "./Canvas.module.css";
 const { wrapper, button } = styles;
-export const Canvas = () => {
-  return (
-    <div className={wrapper}>
-      <div>
-        <canvas></canvas>
+interface State {
+  ref: null | HTMLCanvasElement;
+  ctx: null | CanvasRenderingContext2D;
+  elements: null | number[];
+  drawing: boolean;
+}
+export class Canvas extends Component<{}, State> {
+  BIAS_X = 136;
+  BIAS_Y = 64 + 27;
+  canvasRef = createRef<HTMLCanvasElement>();
+  state: State = {
+    ref: null,
+    ctx: null,
+    elements: null,
+    drawing: false,
+  };
+  componentDidMount = () => {
+    const ref = this.canvasRef.current;
+    if (!ref) return;
+    this.setState({ ref });
+  };
+  componentDidUpdate = () => {
+    const { ctx, ref } = this.state;
+    if (ctx === null && ref !== null) {
+      const ctx = ref.getContext("2d");
+      this.setState({ ctx });
+    }
+  };
+
+  hendleClick = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    this.setState(() => {
+      return { drawing: !this.state.drawing };
+    });
+  };
+  hendleContextMenu = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({ drawing: false });
+    return false;
+  };
+  hendleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    const { BIAS_X, BIAS_Y, state } = this;
+    if (!state.drawing) return;
+    const { clientX, clientY } = e;
+    console.log(clientX - BIAS_X, clientY - BIAS_Y);
+  };
+  render(): React.ReactNode {
+    const { hendleClick, hendleMouseMove, hendleContextMenu, canvasRef } = this;
+    return (
+      <div className={wrapper}>
+        <div>
+          <canvas
+            ref={canvasRef}
+            onMouseMove={hendleMouseMove}
+            onContextMenu={hendleContextMenu}
+            onClick={hendleClick}
+          ></canvas>
+        </div>
+        <button className={button} type="button">
+          collapse lines
+        </button>
       </div>
-      <button className={button} type="button">
-        collapse lines
-      </button>
-    </div>
-  );
-};
+    );
+  }
+}
